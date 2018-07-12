9b849bb1c9646171b0ec6506038b86ac189fbc1a
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2018-07-12 15:23:32 +0200

    Updated plots and script

--- sources/scripts/plot_results.py
+++ sources/scripts/plot_results.py
@@ -4,24 +4,35 @@ from collections import defaultdict
 
 performance = defaultdict(list)
 
-with open('../../results/performance.csv') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',')
+for performance_f in ['performance_laptop.csv', 'performance_xeon.csv']:
+    with open('../../results/' + performance_f) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+        machine = performance_f.split('_')[1].split('.')[0]
 
-    for row in reader:
-        performance['parallelism degree'].append(row['PARALLELISM DEGREE'])
-        performance['completion time'].append(row['COMPLETION TIME'])
-        performance['overhead time'].append(row['OVERHEAD TIME'])
+        for row in reader:
+            performance['parallelism degree ' + machine]. \
+                append(row['PARALLELISM DEGREE'])
+            performance['completion time ' + machine]. \
+                append(row['COMPLETION TIME'])
+            performance['overhead time ' + machine]. \
+                append(row['OVERHEAD TIME'])
 
 for t in ['completion', 'overhead']:
-    plt.plot([int(pd) for pd in performance['parallelism degree']],
-             [float(ct) for ct in performance[t + ' time']], '.-',
-             color='black')
-    plt.xticks([int(pd) for pd in performance['parallelism degree']],
-               performance['parallelism degree'])
+    for machine in ['laptop', 'xeon']:
+        plt.plot([int(pd) for pd in performance['parallelism degree ' +
+                 machine]],
+                 [float(ct) for ct in performance[t + ' time ' + machine]],
+                 '.-' if machine == 'laptop' else '.--',
+                 color='black',
+                 label=machine.upper())
+    plt.xticks([int(pd) for pd in performance['parallelism degree ' +
+               machine]],
+               performance['parallelism degree ' + machine])
     plt.title(t.upper() + ' TIME PER PARALLELISM DEGREE')
     plt.xlabel('PARALLELISM DEGREE')
     plt.ylabel(t.upper() + ' TIME (SECONDS)')
     plt.grid()
+    plt.legend()
     plt.tight_layout()
     plt.savefig('../../report/imgs/' + t + '_time.pdf', format='pdf',
                 bbox_inches='tight')

8af5b6bf89ad71782372c8bbf7505b59821b0715
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2018-07-17 11:04:14 +0200

    Updated plotting script

--- sources/scripts/plot_results.py
+++ sources/scripts/plot_results.py
@@ -3,37 +3,68 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 
 performance = defaultdict(list)
+performance_fastflow = defaultdict(list)
 
-for performance_f in ['performance_laptop.csv', 'performance_xeon.csv']:
-    with open('../../results/' + performance_f) as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=',')
-        machine = performance_f.split('_')[1].split('.')[0]
+for e in ['standard', 'fastflow']:
+    performance_f = ['performance_laptop.csv', 'performance_xeon.csv'] if \
+        e is 'standard' else ['performance_fastflow_laptop.csv',
+                              'performance_fastflow_xeon.csv']
 
-        for row in reader:
-            performance['parallelism degree ' + machine]. \
-                append(row['PARALLELISM DEGREE'])
-            performance['completion time ' + machine]. \
-                append(row['COMPLETION TIME'])
-            performance['overhead time ' + machine]. \
-                append(row['OVERHEAD TIME'])
+    for f in performance_f:
+        with open('../../results/' + f) as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',')
+            machine = f.split('_')[1].split('.')[0] if e is 'standard' else \
+                f.split('_')[2].split('.')[0]
 
-for t in ['completion', 'overhead']:
-    for machine in ['laptop', 'xeon']:
-        plt.plot([int(pd) for pd in performance['parallelism degree ' +
-                 machine]],
-                 [float(ct) for ct in performance[t + ' time ' + machine]],
-                 '.-' if machine == 'laptop' else '.--',
-                 color='black',
-                 label=machine.upper())
-    plt.xticks([int(pd) for pd in performance['parallelism degree ' +
-               machine]],
-               performance['parallelism degree ' + machine])
-    plt.title(t.upper() + ' TIME PER PARALLELISM DEGREE')
-    plt.xlabel('PARALLELISM DEGREE')
-    plt.ylabel(t.upper() + ' TIME (SECONDS)')
-    plt.grid()
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig('../../report/imgs/' + t + '_time.pdf', format='pdf',
-                bbox_inches='tight')
-    plt.clf()
+            for row in reader:
+                if e is 'standard':
+                    performance['parallelism degree ' + machine]. \
+                        append(row['PARALLELISM DEGREE'])
+                    performance['completion time ' + machine]. \
+                        append(row['COMPLETION TIME'])
+                    performance['average latency ' + machine]. \
+                        append(row['AVERAGE LATENCY'])
+                    performance['scalability ' + machine].append(
+                        row['SCALABILITY'])
+                else:
+                    performance_fastflow['parallelism degree ' + machine]. \
+                        append(row['PARALLELISM DEGREE'])
+                    performance_fastflow['completion time ' + machine]. \
+                        append(row['COMPLETION TIME'])
+                    performance_fastflow['average latency ' + machine]. \
+                        append(row['AVERAGE LATENCY'])
+                    performance_fastflow['scalability ' + machine].append(
+                        row['SCALABILITY'])
+
+for parameter in ['completion time', 'average latency', 'scalability']:
+    for e in ['standard', 'fastflow']:
+        for machine in ['laptop', 'xeon']:
+            if e is 'standard':
+                plt.plot([int(pd) for pd in performance['parallelism degree '
+                         + machine]],
+                         [float(p) for p in performance[parameter + ' ' +
+                          machine]], '.-' if machine == 'laptop' else '.--',
+                         color='black', label=machine.upper())
+                plt.xticks([int(pd) for pd in performance['parallelism degree '
+                            + machine]],
+                           performance['parallelism degree ' + machine])
+            else:
+                plt.plot([int(pd) for pd in performance_fastflow[
+                         'parallelism degree ' + machine]],
+                         [float(p) for p in performance_fastflow[parameter +
+                          ' ' + machine]],
+                         '.-' if machine == 'laptop' else '.--',
+                         color='black', label=machine.upper())
+                plt.xticks([int(pd) for pd in performance_fastflow[
+                           'parallelism degree ' + machine]],
+                           performance['parallelism degree ' + machine])
+        plt.title(parameter.upper() + ' PER PARALLELISM DEGREE')
+        plt.xlabel('PARALLELISM DEGREE')
+        plt.ylabel(parameter.upper() + ' (SECONDS)')
+        plt.grid()
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig('../../report/imgs/' + parameter.replace(' ', '_') +
+                    '_' + e + '.pdf',
+                    format='pdf', bbox_inches='tight')
+        plt.clf()
